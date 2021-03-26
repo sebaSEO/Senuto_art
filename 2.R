0406f0727c8e4ef29687abbd5876746588d97c35
sebaSEO <50496595+sebaSEO@users.noreply.github.com>
2021-03-26 22:12:35 +0100

    Update 2.R

--- 2.R
+++ 2.R
@@ -1,3 +1,10 @@
+packages <- c(" googleAnalyticsR")
+
+
+install.packages(setdiff(packages, rownames(installed.packages())))
+
+
+
 #Do obiektu o nazwie id_widoku pobieramy ID widoków Google Analytics do których mamy dostep
 my_acc <- ga_account_list()
 id_widoku <- my_acc$viewId
@@ -9,7 +16,8 @@ zakresu dat od 12 dni wstecz do wczoraj
 liczby użytkowników i sesji
 danych w podziale na dni
 
-ga_results1 <-google_analytics(ga_id,
+dane_GA <-google_analytics(viewId=id_widoku,
                                date_range = c("12daysAgo", "yesterday"),
-                               metrics = c("users","sessions"),
+                               metrics = c("users","newUsers"),
                                dimensions = "date")
+
