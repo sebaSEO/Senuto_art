3cebf8d12e0a6dee74b242c69eeedd202839a2ca
sebaSEO <50496595+sebaSEO@users.noreply.github.com>
2021-03-26 15:45:46 +0100

    Update 1.R

--- 1.R
+++ 1.R
@@ -1,3 +1,11 @@
 # Cały kod użyty w artykule
 
-#Pierwszy listing
+#Krok 1
+packages <- c("searchConsoleR"," googleAnalyticsR")
+
+#Krok 2
+install.packages(setdiff(packages, rownames(installed.packages())))
+
+
+#Krok 3
+scr_auth()
