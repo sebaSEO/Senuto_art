5f9bba3715d409ed8e0a2dc952d7c0abeb958e4f
sebaSEO <50496595+sebaSEO@users.noreply.github.com>
2021-03-26 21:53:11 +0100

    Update 2.R

--- 2.R
+++ 2.R
@@ -1,3 +1,15 @@
 #Do obiektu o nazwie id_widoku pobieramy ID widoków Google Analytics do których mamy dostep
 my_acc <- ga_account_list()
 id_widoku <- my_acc$viewId
+
+
+#Pobieramy do obeiktu o nazwie ga_results1 dane dotyczące:
+
+zakresu dat od 12 dni wstecz do wczoraj
+liczby użytkowników i sesji
+danych w podziale na dni
+
+ga_results1 <-google_analytics(ga_id,
+                               date_range = c("12daysAgo", "yesterday"),
+                               metrics = c("users","sessions"),
+                               dimensions = "date")
